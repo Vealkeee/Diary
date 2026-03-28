4f999801544f2ea1c72d4e22e22e92603e826626
Vealkeee <koristaosu1337@gmail.com>
2026-03-28 21:55:46 +0400

    database initialized and structured directory

---
+++ src/db/engine.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from config import settings
+
+URL = settings.psycopg_GET_DB
+
+engine = create_engine(URL, echo=True)
+localSession = sessionmaker(engine)
