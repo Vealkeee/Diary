452e55b3a389e9da98a0189d6db1334dad397021
Vealkeee <koristaosu1337@gmail.com>
2026-03-30 21:05:50 +0400

    rate limiter middleware implemented

---
+++ src/api/middleware/RateLimit.py
@@ -0,0 +1,31 @@
+from fastapi import Request, HTTPException
+from starlette.middleware.base import BaseHTTPMiddleware
+from time import time
+
+request_counts = {}
+RATE_LIMIT = 5  # Max 5 requests
+TIME_WINDOW = 60
+
+class RateLimitMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+            
+            client_ip = request.client.host
+            current_time = time()
+
+            if client_ip in request_counts:
+                    request_times = request_counts[client_ip]
+
+                    # Remove outdated requests outside the time window
+                    request_counts[client_ip] = [
+                        timestamp for timestamp in request_times if current_time - timestamp < TIME_WINDOW
+                    ]
+
+                    if len(request_counts[client_ip]) >= RATE_LIMIT:
+                        raise HTTPException(status_code=429, detail="Too Many Requests")
+
+            # Add current request timestamp
+            request_counts.setdefault(client_ip, []).append(current_time)
+
+            # Proceed to the next middleware or endpoint
+            response = await call_next(request)
+            return response
