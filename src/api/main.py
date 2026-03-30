706619785450156c75eff14befbfa01c440e3579
Vealkeee <koristaosu1337@gmail.com>
2026-03-30 21:05:21 +0400

    api initializing stuff, logging

--- src/api/main.py
+++ src/api/main.py
@@ -3,11 +3,20 @@ import logging
 
 from fastapi import FastAPI
 
-logging.basicConfig(level=logging.INFO, filename="../../logs/runtimeLog.log", filemode='a',
+from middleware.RateLimit import RateLimitMiddleware
+from endpoints.getStudent import router as getStudent
+
+logging.basicConfig(level=logging.INFO, filename="./logs/runtimeLog.log", filemode='a',
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = FastAPI()
 
+app.add_middleware(RateLimitMiddleware)
+app.include_router(getStudent)
+
 if __name__ == "__main__":
-    logging.info("Starting the API server")
-    uvicorn.run(app, host="127.0.0.1", port=7272)
+    try:
+        logging.info("Starting the API server")
+        uvicorn.run(app, host="127.0.0.1", port=7272)
+    except Exception as e:
+        logging.error(f"An error occurred while starting or developing the API server: {e}")
