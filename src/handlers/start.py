2dd79d8c84ad67036679bb92d10beebbfecc4a11
Vealkeee <koristaosu1337@gmail.com>
2026-04-01 09:58:17 +0400

    userExistance check

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -2,6 +2,9 @@ from aiogram.types import Message
 from aiogram.filters import CommandStart
 from aiogram.enums import ParseMode
 
+from sqlalchemy import select
+from src.db.models import Student
+
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from aiogram import Router, F
@@ -9,11 +12,23 @@ from aiogram import Router, F
 router = Router()
 
 @router.message(CommandStart())
-async def welcome(message: Message):
+async def welcome(message: Message, db_pool):
 
-    kb = InlineKeyboardBuilder()
-    kb.button(text="✅", callback_data="agreement")
-    start_kb = kb.as_markup()
+    with db_pool() as session:
 
-    await message.answer('💼 <b>ДОБРО ПОЖАЛОВАТЬ!</b>\n\nВ электронный журнал "Астраханского Колледжа Вычислительной Техники"!\n\n'\
-                         'Если вы готовы приступить к регистрации, нажмите на галочку.', parse_mode=ParseMode.HTML, reply_markup=start_kb)
+        userID = message.from_user.id
+        stmt = (select(Student.tgID).
+                where(Student.tgID == userID))
+        
+        scalarID = session.scalar(stmt)
+
+    if not scalarID:
+        kb = InlineKeyboardBuilder()
+        kb.button(text="✅", callback_data="agreement")
+        start_kb = kb.as_markup()
+
+        await message.answer('💼 <b>ДОБРО ПОЖАЛОВАТЬ!</b>\n\nВ электронный журнал "Астраханского Колледжа Вычислительной Техники"!\n\n'\
+                            'Если вы готовы приступить к регистрации, нажмите на галочку.', parse_mode=ParseMode.HTML, reply_markup=start_kb)
+    else:
+        await message.answer("<b>❌ ОТКАЗАНО</b>\n\nВы уже <b>АВТОРИЗОВАНЫ.</b>", parse_mode=ParseMode.HTML)
+       
