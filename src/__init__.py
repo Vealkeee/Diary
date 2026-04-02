53fff08867ade14ed3efd728af49690694f020cc
Vealkeee <koristaosu1337@gmail.com>
2026-04-02 17:30:52 +0400

    back router and headman router

--- src/__init__.py
+++ src/__init__.py
@@ -3,10 +3,14 @@ __all__ = ("router")
 from aiogram import Router
 from src.handlers import start
 from src.handlers.registration import registration
+from src.handlers.registration.locationStates import backToRegister
+from src.handlers.registration.headmans import newHeadman
 
 router = Router()
 
 router.include_routers(
     start.router,
-    registration.router
+    registration.router,
+    backToRegister.router,
+    newHeadman.router
 )
