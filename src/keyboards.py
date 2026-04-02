8919e1969f3269f38db977445f10ba178ec13f64
Vealkeee <koristaosu1337@gmail.com>
2026-04-02 22:27:03 +0400

    keyboards added

--- src/keyboards.py
+++ src/keyboards.py
@@ -1,2 +1,19 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import (
+    ReplyKeyboardMarkup,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton
+)
 
+headman_main = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text="🧮 Успеваемость", callback_data="grades_status_hd"),
+            InlineKeyboardButton(text="📝 Домашняя работа", callback_data="homework_hd")
+        ],
+        [
+            InlineKeyboardButton(text="🗓️ Расписание", callback_data="schedule_hd"),
+            InlineKeyboardButton(text="✅ Привязки", callback_data="studentConnections")
+        ]
+    ]
+)
