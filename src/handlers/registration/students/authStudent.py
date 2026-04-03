19b9e57edbb0bd2e277763b9a02c0e62fe9cff87
Vealkeee <koristaosu1337@gmail.com>
2026-04-03 18:36:35 +0400

    variables but better

--- src/handlers/registration/students/authStudent.py
+++ src/handlers/registration/students/authStudent.py
@@ -1,6 +1,4 @@
-from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import Message, CallbackQuery
-from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
 from aiogram.enums import ParseMode
 from aiogram import Router, F
 
@@ -42,30 +40,24 @@ async def CreateConnectionKB(call: CallbackQuery, db_pool):
                 where(Group.group_name == group_name)
             )
 
-            headmanName = db.execute(stmt2).all()[0][0]
-            headmanSecondName = db.execute(stmt2).all()[0][1]
-            HeadmanTGid = db.execute(stmt2).all()[0][2]
-            kb.button(text=f"{headmanName} {headmanSecondName}", callback_data=f"{HeadmanTGid}")
-            keyboard = kb.as_markup()
-
-            await call.message.answer("<b>🎄 ПРИВЯЗКА</b>\n\nДля <b>отправки привязки</b> к старосте, нажмите снизу на его инициалы... Через <b>24 часа</b> введите комманду старт...", parse_mode=ParseMode.HTML, reply_markup=keyboard)
+            try:
+                headmanName, headmanSecondName, HeadmanTGid = db.execute(stmt2).first()
+                kb.button(text=f"{headmanName} {headmanSecondName}", callback_data=f"connect:{HeadmanTGid}")
+                keyboard = kb.as_markup()
+                await call.message.answer("<b>🎄 ПРИВЯЗКА</b>\n\nДля <b>отправки привязки</b> к старосте, нажмите снизу на его инициалы... Через <b>24 часа</b> введите комманду старт...", parse_mode=ParseMode.HTML, reply_markup=keyboard)
+            except Exception:
+                await call.message.answer("<b>🎄 ПРИВЯЗКА</b>\n\nВ вашей группе отсутствует староста... Попробуйте позже.", parse_mode=ParseMode.HTML)
         elif status == True:
             await call.message.answer("<b>🎄 ПРИВЯЗКА</b>\n\nАккаунт уже <b>привязан</b> к старосте...", parse_mode=ParseMode.HTML)
         else:
             await call.message.answer("<b>🎄 ПРИВЯЗКА</b>\n\nАккаунт находится в процессе привязки...", parse_mode=ParseMode.HTML)
         
-@router.callback_query()
+@router.callback_query(F.data.contains("connect:"))
 async def connectStudent(call: CallbackQuery, db_pool):
 
     await call.answer()
-    
+
     with db_pool() as db:
-
-        stmt = select(Group.headman_tgID)
-        valid_ids = [row[0] for row in db.execute(stmt)]
-
-        if call.data not in map(str, valid_ids):
-            return  # ignore unrelated callbacks
 
         updateStudentState = (
             update(Student).
